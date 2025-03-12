3064bce4cf68caf273d9202466aced734a461ad6
mherczeg <herczeg.mt@gmail.com>
2025-03-12 09:42:40 +0100

    change alarm time to consider the set time the end of the sunrise

--- scripts/check-alarm.sh
+++ scripts/check-alarm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Load environment variables from the .env file located one level above the current directory
+source "$(dirname "$0")/../.env"
+
 SCRIPT_DIR=$(dirname "$0")
 
 # Path to alarm.json
@@ -10,12 +13,12 @@ ALARM_TIME=$(jq -r '.alarmTime' "$ALARM_FILE")
 ALARM_ENABLED=$(jq -r '.alarmEnabled' "$ALARM_FILE")
 
 # Get the current time in HH:MM format
-CURRENT_TIME=$(date +"%H:%M")
+END_TIME=$(date -d "+$SUNRISE_DURATION minutes" +"%H:%M:%S")
 
-# Check if the alarm is enabled and the current time matches alarmTime
-if [[ "$ALARM_ENABLED" == "true" && "$CURRENT_TIME" == "$ALARM_TIME" ]]; then
+# Check if the alarm is enabled and the alarm end time matches alarmTime
+if [[ "$ALARM_ENABLED" == "true" && "$ALARM_TIME" == "$END_TIME" ]]; then
     echo "Alarm time reached! Running the commands..."
-    ./$SCRIPT_DIR/check-alarm.sh &
+    ./$SCRIPT_DIR/start-alarm.sh &
 else
-    echo "No alarm triggered. Current time is $CURRENT_TIME. Alarm time is $ALARM_TIME. The alarm status is $ALARM_ENABLED"
+    echo "No alarm triggered. The alarm end time would be $END_TIME. Alarm time is $ALARM_TIME. The alarm status is $ALARM_ENABLED"
 fi
